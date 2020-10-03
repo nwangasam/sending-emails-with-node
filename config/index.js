d8c933c1e291804f2703075f2f33f0c69d69657b
nwangasam <46712385+nwangasam@users.noreply.github.com>
2020-10-03 13:23:46 +0100

    Fixed little syntax bug

--- config/index.js
+++ config/index.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
     PASSWORD: process.env.PASSWORD,
     EMAIL: process.env.EMAIL,
-    MAIN_URL: proess.env.MAIN_URL
+    MAIN_URL: process.env.MAIN_URL
 }
